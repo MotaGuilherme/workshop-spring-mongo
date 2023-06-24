2e4b4b060f4e675d6e3028901558d30e9c258632
MotaGuilherme <metatronmota@gmail.com>
2023-06-24 02:23:15 -0300

    getting user by id

--- src/main/java/example/resources/services/UserService.java
+++ src/main/java/example/resources/services/UserService.java
@@ -2,10 +2,12 @@ package example.resources.services;
 
 import example.resources.domain.User;
 import example.resources.repository.UserRepository;
+import example.resources.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
