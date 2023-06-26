5e4daf22d85d5e93d45a33e9b6431a4221e5c49e
MotaGuilherme <metatronmota@gmail.com>
2023-06-26 19:49:34 -0300

    get posts by id

---
+++ src/main/java/example/resources/services/PostService.java
@@ -0,0 +1,21 @@
+package example.resources.services;
+
+import example.resources.domain.Post;
+import example.resources.repository.PostRepository;
+import example.resources.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+        public Post findById (String id) {
+            Optional<Post> obj = repo.findById(id);
+            return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        }
+    }
