dfdf7d9020bbb663083b7e28e08d4a14641a7e4b
MotaGuilherme <metatronmota@gmail.com>
2023-06-26 20:20:04 -0300

    Consulting with query methods

--- src/main/java/example/resources/repository/PostRepository.java
+++ src/main/java/example/resources/repository/PostRepository.java
@@ -4,8 +4,10 @@ import example.resources.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
-
+    List<Post> findByTitleIsContainingIgnoreCase(String text);
 }
--- src/main/java/example/resources/resources/PostResource.java
+++ src/main/java/example/resources/resources/PostResource.java
@@ -1,10 +1,13 @@
 package example.resources.resources;
 
 import example.resources.domain.Post;
+import example.resources.resources.util.URL;
 import example.resources.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -15,10 +18,15 @@ public class PostResource {
     private PostService service;
 
       @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Post> findById(@PathVariable String id) {
+        public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
 
-
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+          text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+          return ResponseEntity.ok().body(list);
+    }
 }
